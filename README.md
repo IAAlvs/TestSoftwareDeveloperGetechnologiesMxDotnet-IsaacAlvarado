89f583c338394c311e49ed3fbfc2ee69080fff81
IAALvs <isaacalf.alvs@gmail.com>
2023-12-09 17:06:02 -0600

    add readme and change swagger ui url

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# TestSoftwareDeveloperGetechnologiesMxDotnet-IsaacAlvarado
-TestSoftwareDeveloperGetechnologiesMxDotnet
+API Test Ventas
+=============
+
+Instrucciones para ejecutar la Api
+-----------------------------------------
+
+Para ejecutar la API de Ventas, sigue los siguientes pasos:
+
+1.  Abre una terminal o línea de comandos en la carpeta raíz del proyecto.
+2.  Ejecuta el siguiente comando para restaurar las dependencias:
+
+`dotnet restore`
+
+3.  Ejecuta el siguiente comando para aplicar las migraciones de base de datos:
+
+`dotnet ef database update`
+
+4.  Finalmente, ejecuta la aplicación con el siguiente comando:
+
+`dotnet run`
+
+La API estará disponible en [http://localhost:5000](http://localhost:5000).
+
+Explorar la API en Swagger
+--------------------------
+
+Para explorar y probar los endpoints de la API, abre tu navegador y visita:
+
+`http://localhost:7064/api/v1/sales/swagger/index.html`
+
+Desde la interfaz de Swagger, podrás ver la documentación detallada y realizar pruebas en vivo de la API de Ventas.
+
+### Notas importantes:
+
+*   Asegúrate de tener .NET SDK instalado en tu máquina antes de ejecutar la aplicación.
+*   Si la aplicación está configurada en un puerto diferente, ajusta las URL en consecuencia.
--- src/SalesApi/Program.cs
+++ src/SalesApi/Program.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using SalesApi.AspectDefinitions;
 using SalesApi.EndpointDefinition;
 using SalesApi.Repositories;
 using Serilog;
@@ -11,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, loggerConfig) => 
     loggerConfig.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
+    
+CorsAspectDefinition.DefineAspect(builder.Services, builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,15 +25,23 @@ PersonsEndpointDefinition.DefineServices(builder.Services);
 InvoicesEndpointDefinition.DefineServices(builder.Services);
 
 var app = builder.Build();
+CorsAspectDefinition.ConfigureAspect(app);
 
 PersonsEndpointDefinition.DefineEndpoints(app);
 InvoicesEndpointDefinition.DefineEndpoints(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+{       const string apiVersion = "v1";
+        app.UseSwagger(options =>
+        {
+            options.RouteTemplate = "api/"+apiVersion+"/sales/swagger/{documentname}/swagger.json";
+        });
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint($"{apiVersion}/swagger.json", "Sales test api");
+            options.RoutePrefix = $"api/{apiVersion}/sales/swagger";
+        });
 }
 
 app.UseSerilogRequestLogging();
